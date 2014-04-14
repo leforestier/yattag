79cc9420886f32a62e2fff26511aa88b9ea91850
Benjamin Le Forestier <leforestier@users.noreply.github.com>
2014-04-14 10:44:40 +0200

    version

--- yattag/__init__.py
+++ yattag/__init__.py
@@ -55,7 +55,7 @@ Full tutorial on yattag.org_
 """
 
 __author__ = "Benjamin Le Forestier (benjamin@leforestier.org)"
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 from yattag.simpledoc import SimpleDoc
 from yattag.doc import Doc
