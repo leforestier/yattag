504eeb8e947fb30f17044e41a0b48f54c7b19961
Benjamin Le Forestier <leforestier@users.noreply.github.com>
2014-09-12 14:41:45 +0200

    version 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as fd:
 
 setup(
     name='yattag',
-    version='1.0.5',
+    version='1.0.6',
     packages=['yattag'],
     author = 'Benjamin Le Forestier',
     author_email = 'benjamin@leforestier.org',
--- yattag/__init__.py
+++ yattag/__init__.py
@@ -55,7 +55,7 @@ Full tutorial on yattag.org_
 """
 
 __author__ = "Benjamin Le Forestier (benjamin@leforestier.org)"
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 from yattag.simpledoc import SimpleDoc
 from yattag.doc import Doc
