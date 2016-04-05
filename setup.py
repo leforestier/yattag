5650a17a37b8a2a93c7882d46d784371bcfe05b6
Benjamin Le Forestier <leforestier@users.noreply.github.com>
2016-04-05 23:54:10 +0200

    version 1.5.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as fd:
 
 setup(
     name='yattag',
-    version='1.5.2',
+    version='1.5.3',
     packages=['yattag'],
     author = 'Benjamin Le Forestier',
     author_email = 'benjamin@leforestier.org',
